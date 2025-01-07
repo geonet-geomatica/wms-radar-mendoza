418b20a4042e90a6d068170f057ae5809ef1c8b1
geonet-geomatica <geonet.geomatica@gmail.com>
2025-01-07 15:04:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -79,6 +79,6 @@ def get_capabilities():
     </WMS_Capabilities>"""
     return Response(capabilities, mimetype="application/xml")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
