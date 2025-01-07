55f4b145cab4b36b824f4dba77d22c4fa222da08
geonet-geomatica <geonet.geomatica@gmail.com>
2025-01-07 01:45:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,59 @@
 from flask import Flask, request, Response
 from owslib.wms import WebMapService
-import requests
-from io import BytesIO
+from owslib.fei import WMSCapabilities
 
 app = Flask(__name__)
 
-# Definir la URL base para las imágenes del radar
-RADAR_URL = "https://www2.contingencias.mendoza.gov.ar/radar/muestraimagen.php"
-
 @app.route('/wms', methods=['GET'])
-def wms():
-    # Obtener los parámetros WMS (por ejemplo, BBOX, WIDTH, HEIGHT)
-    bbox = request.args.get('BBOX', '-37.40959444444444,-71.71962222222223,-31.22909166666667,-65.02164166666667')
-    imagen = request.args.get('imagen', 'google.png')
-
-    # Construir la URL con los parámetros
-    url = f"{RADAR_URL}?imagen={imagen}&sw={bbox.split(',')[0]},{bbox.split(',')[1]}&ne={bbox.split(',')[2]},{bbox.split(',')[3]}&centro=-34.0,-68.4&zoom=7"
+def wms_service():
+    service = request.args.get('SERVICE')
+    request_type = request.args.get('REQUEST')
     
-    # Realizar la solicitud HTTP para obtener la imagen
-    response = requests.get(url)
-    
-    # Comprobar si la solicitud fue exitosa
-    if response.status_code == 200:
-        img = BytesIO(response.content)
-        return Response(img, mimetype='image/png')
-    else:
-        return "Error obteniendo la imagen del radar", 500
+    if service == 'WMS':
+        if request_type == 'GetCapabilities':
+            return get_capabilities()
+        elif request_type == 'GetMap':
+            return get_map()
+        else:
+            return "Unknown request type", 400
+    return "Invalid WMS service", 400
 
-# Ejecutar la aplicación Flask
+def get_capabilities():
+    # Aquí puedes definir los parámetros de tu servicio WMS
+    capabilities = """<?xml version="1.0" encoding="UTF-8"?>
+    <WMS_Capabilities version="1.3.0">
+        <Service>
+            <Name>WMS</Name>
+            <Title>Radar Mendoza</Title>
+            <Abstract>Servicio WMS para mostrar radar de Mendoza</Abstract>
+            <KeywordList>
+                <Keyword>Radar</Keyword>
+                <Keyword>Mendoza</Keyword>
+            </KeywordList>
+            <OnlineResource xlink:type="simple" xlink:href="https://<nombre-de-tu-app>.onrender.com/wms" />
+        </Service>
+        <Capability>
+            <Request>
+                <GetCapabilities>
+                    <Format>application/xml</Format>
+                </GetCapabilities>
+                <GetMap>
+                    <Format>image/png</Format>
+                    <Layer queryable="1">
+                        <Name>radar</Name>
+                    </Layer>
+                </GetMap>
+            </Request>
+        </Capability>
+    </WMS_Capabilities>"""
+    return Response(capabilities, mimetype="application/xml")
+
+def get_map():
+    # Aquí iría el código para generar la imagen solicitada en la capa WMS
+    # Por ejemplo, podrías servir una imagen PNG.
+    image_url = "https://www2.contingencias.mendoza.gov.ar/radar/muestraimagen.php?imagen=google.png&sw=-37.40959444444444,-71.71962222222223&ne=-31.22909166666667,-65.02164166666667&centro=-34.0,-68.4&zoom=7"
+    img_response = requests.get(image_url)
+    return Response(img_response.content, mimetype="image/png")
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
