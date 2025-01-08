8eb51f280e6f739611aa91e46b08abaf51c63e93
geonet-geomatica <geonet.geomatica@gmail.com>
2025-01-08 00:17:16 -0300

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ def get_capabilities():
             <Abstract>Servicio WMS para mostrar radar de Mendoza</Abstract>
             <CRS>EPSG:4326</CRS>
             <CRS>EPSG:3857</CRS>
-            <BoundingBox CRS="EPSG:4326" minx="-71.7249353229025530" miny="-37.4356023471214812" maxx="-64.9942298546511239" maxy="-31.2320003191547890" />
-            <BoundingBox CRS="EPSG:3857" minx="-7984383.2773259934037924" miny="-4499999.3192690527066588" maxx="-7235124.5719207422807813" maxy="-3662915.5957877929322422" />
+            <BoundingBox CRS="EPSG:4326" minx="-37.4356023471214812" miny="-71.7249353229025530" maxx="-31.2320003191547890" maxy="-64.9942298546511239" />
+            <BoundingBox CRS="EPSG:3857" minx="-4499999.3192690527066588" miny="-7984383.2773259934037924" maxx="-3662915.5957877929322422" maxy="-7235124.5719207422807813" />
         </Service>
         <Capability>
             <Request>
